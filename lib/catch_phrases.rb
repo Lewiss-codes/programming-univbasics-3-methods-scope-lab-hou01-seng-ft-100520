6d3693c9990f7d964347b4142dc227c7029f8170
Luis Penaloza <ldp231@yahoo.com>
2020-09-08 04:16:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,5 +5,9 @@ end
 
 def toadstool
   puts status
-  
+  describe "mario" do
+  it "puts out 'It's-a me, Mario!'" do
+    phrase = "It's-a me, Mario!"
+    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+  end
 end
